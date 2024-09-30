bc65eab6014098b52198c3f3e9ad9b9f8aaec3d5
Li Zhineng <im@zhineng.li>
2024-09-30 16:59:57 +0800

    fix sidebar height

--- resources/views/reference/sidebar.blade.php
+++ resources/views/reference/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="fixed pt-8 pb-16 w-80 bg-slate-50 overflow-auto overscroll-y-contain max-h-screen">
+<aside class="fixed pt-8 pb-16 w-80 bg-slate-50 overflow-auto overscroll-y-contain h-screen">
     @foreach ($directories as $directory)
         <x-sidebar.navigation
             :definition="$directory"
