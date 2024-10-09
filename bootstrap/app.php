444a5d031f89a27c64db38de10e1475c86c974cc
Li Zhineng <im@zhineng.li>
2024-10-09 20:45:57 +0800

    remove cookies and session

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            ConfigureLanguage::class,
-        ]);
+        $middleware->web(
+            append: [
+                ConfigureLanguage::class,
+            ],
+            remove: [
+                \Illuminate\Cookie\Middleware\EncryptCookies::class,
+                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                \Illuminate\Session\Middleware\StartSession::class,
+                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            ],
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
