81f6c7b47c20f5d7280074a619aeaea4ff227336
Li Zhineng <im@zhineng.li>
2024-10-09 21:38:42 +0800

    delete language controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReferenceController;
 use Illuminate\Support\Facades\Route;
@@ -12,4 +11,3 @@ Route::get('/', function () {
 Route::get('/{locale}/{product}', [ProductController::class, 'show'])->name('references.product.show');
 Route::get('/{locale}/{product}/{version}', [ReferenceController::class, 'index'])->name('references.apis.index');
 Route::get('/{locale}/{product}/{version}/{api}', [ReferenceController::class, 'show'])->name('references.apis.show');
-Route::post('/languages', [LanguageController::class, 'store']);
