23c5babed6330eafbd55b65325a37edbe00a1b9b
Li Zhineng <im@zhineng.li>
2024-10-09 21:41:57 +0800

    redirect temporarilly

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReferenceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/en-us/vpc');
 
 Route::get('/{locale}/{product}', [ProductController::class, 'show'])->name('references.product.show');
 Route::get('/{locale}/{product}/{version}', [ReferenceController::class, 'index'])->name('references.apis.index');
