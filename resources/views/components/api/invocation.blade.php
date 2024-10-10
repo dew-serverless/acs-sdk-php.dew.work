cb58ed5b8eff0168580ab243e37f7dad83fb886e
Li Zhineng <im@zhineng.li>
2024-10-10 22:25:00 +0800

    rename variable

--- resources/views/components/api/invocation.blade.php
+++ resources/views/components/api/invocation.blade.php
@@ -4,7 +4,7 @@
 ])
 
 @php
-    $methodBg = match (strtoupper($method)) {
+    $bg = match (strtoupper($method)) {
         'GET' => 'bg-emerald-600',
         'POST' => 'bg-cyan-600',
         'PUT' => 'bg-blue-600',
@@ -16,7 +16,7 @@
 
 <div class="leading-none">
     <div class="inline-flex items-center pl-4 pr-6 py-3 text-sm text-white bg-slate-100 rounded-md">
-        <div class="px-5 py-2 text-center {{ $methodBg }} leading-none rounded-md">
+        <div class="px-5 py-2 text-center {{ $bg }} leading-none rounded-md">
             {{ $method }}
         </div>
 
